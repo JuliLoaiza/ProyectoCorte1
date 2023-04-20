c7d49d33ad5a39ba767bd611edf9d608e398429c
= <100366113+AnaMontanoM@users.noreply.github.com>
2023-04-20 15:24:23 -0500

    Arreglo Home Pages

---
+++ client/src/Pages/HomeC/index.jsx
@@ -0,0 +1,30 @@
+import InputC from "../../Components/InputC";
+import ButtonC from "../../Components/ButtonC";
+import bubbles from "../../img/bubbles.jpg";
+import "../HomeP/index.css";
+import ServiceCardC from "../../Components/ServiceCardC";
+
+const HomeC = () => {
+	return (
+		<main className="InputsM">
+			<section>
+				<div class="contentBx">
+					<div className="Titulo">
+						<h1>Home</h1>
+					</div>
+					<div>
+						<ServiceCardC />
+						<ServiceCardC />
+						<ServiceCardC />
+						<ServiceCardC />
+					</div>
+				</div>
+				<div class="imgBx">
+					<img src={bubbles} alt="" />
+				</div>
+			</section>
+		</main>
+	);
+};
+
+export default HomeC;
